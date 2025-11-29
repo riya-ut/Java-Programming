9e9ae97209e2ebae1207d03ed48705d99b454e87
Riya UT <156648774+riya-ut@users.noreply.github.com>
2025-11-29 09:29:21 +0530

    Add files via upload

---
+++ LESSON1.java
@@ -0,0 +1,12 @@
+public class LESSON1 {
+
+    public static void main(String[] args) {
+        System.out.println("  **     **  ");
+        System.out.println("****     ****");
+        System.out.println(" *********** ");
+        System.out.println("   *******   ");
+        System.out.println("    *****    ");
+        System.out.println("     ***     ");
+        System.out.println("      *      ");
+    }
+}
---
+++ PrimitiveDataTypes2.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataTypes2 {
+    public static void main(String[] args) {
+        byte smallNumber = 127;
+        short mediNumber = 32000;
+        int number = 100000;
+        long largeNumber = 123456789L;
+        float decimalvalue = 10.5f;
+        double largeDecimalValue = 12345.6789;
+        char intial = 'v';
+        boolean isJavaFun = true;
+        System.out.println("Byte: " + smallNumber);
+        System.out.println("Short:" + mediNumber);
+        System.out.println("Inte: " + number);
+        System.out.println("Long:" + largeNumber);
+        System.out.println("Float" + decimalvalue);
+        System.out.println("Double:" + largeDecimalValue);
+        System.out.println("char: " + intial);
+        System.out.println("Boolean" + isJavaFun);
+    }
+}
